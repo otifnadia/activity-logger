1a9090f52ab78298d1b6ff5fa819a94fe6c46738
Nadia <nadianazar2422@gmail.com>
2025-06-12 10:48:21 +0500

    Fix: removed self-require from composer.json

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,29 @@ class ComposerAutoloaderInitf06d5090f228d3a5825ce25f35b75fc7
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitf06d5090f228d3a5825ce25f35b75fc7::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequiref06d5090f228d3a5825ce25f35b75fc7($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequiref06d5090f228d3a5825ce25f35b75fc7($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
